6e89ded00b5edbefa1473d78fc6064e90a526c82
Piotr Sarnacki <drogus@gmail.com>
2023-02-13 16:53:25 +0100

    Updates

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p ~/.config
 ln -sf $SCRIPT_DIR/nvim $HOME/.config/nvim
 ln -sf $SCRIPT_DIR/zsh $HOME/.zsh
+# TODO: probably good to ask
+rm -fr $HOME/.zshrc
 ln -sf $SCRIPT_DIR/zshrc $HOME/.zshrc
 ln -sf $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
